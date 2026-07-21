e231f245489cb2e761b0c7691dd1df9d956975f2
abra-adl <sxawra1997@gmail.com>
2026-07-21 21:49:44 +0100

    seperated username from Email in UserRepository

--- src/main/java/com/abrar/BOOKSTORE/Login/user/UserRepository.java
+++ src/main/java/com/abrar/BOOKSTORE/Login/user/UserRepository.java
@@ -1,19 +1,19 @@
 package com.abrar.BOOKSTORE.Login.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByUsernameOrEmail(String username);
 
-    Optional<User> findByUsername(String username);
+    @Query("SELECT u FROM User u WHERE u.usernameOrEmail = :usernameOrEmail")
+    Optional<User> findByUsernameOrEmail(@Param("usernameOrEmail") String usernameOrEmail);
 
-    Optional<User> findByEmail(String email);
-
-    boolean existsByUsernameOrEmail(String username, String email);
-
-    // can define custom queries or methods here if needed
+    @Query("SELECT CASE WHEN COUNT(u) > 0 THEN true ELSE false END FROM User u " +
+            "WHERE u.usernameOrEmail = :username OR u.usernameOrEmail = :email")
+    boolean existsByUsernameOrEmail(@Param("username") String username, @Param("email") String email);
 }
