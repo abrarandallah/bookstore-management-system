ab3b7d01c79dc1f129a7ac7636c42a62daf0de33
abra-adl <sxawra1997@gmail.com>
2026-07-23 21:42:45 +0100

    Improve service error handling for missing books

--- src/main/java/com/abrar/BOOKSTORE/service/BookService.java
+++ src/main/java/com/abrar/BOOKSTORE/service/BookService.java
@@ -1,6 +1,7 @@
 package com.abrar.BOOKSTORE.service;
 
 import com.abrar.BOOKSTORE.entity.Book;
+import com.abrar.BOOKSTORE.exception.ResourceNotFoundException;
 import com.abrar.BOOKSTORE.repository.BookRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,20 +14,37 @@ public class BookService {
 
     @Autowired
     private BookRepository bRepo;
-    public void save(Book b){
+
+    public void save(Book b) {
         bRepo.save(b);
     }
-    public List<Book> getAllBook(){
+
+    public List<Book> getAllBook() {
         return bRepo.findAll();
     }
-    public Book getBookById(int id){
-        if (bRepo.findById(id).isPresent()) {
-            return bRepo.findById(id).get();
-        } else {
-            return null;
-        }
+
+    /**
+     * @throws ResourceNotFoundException if no book exists with the given id.
+     *                                   Previously this returned null, which caused
+     *                                   NullPointerExceptions in
+     *                                   every caller (editBook, getMylist, etc.)
+     *                                   whenever an id didn't exist.
+     */
+    public Book getBookById(int id) {
+        return bRepo.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Book not found with id: " + id));
     }
-    public void deleteById(int id){
+
+    /**
+     * @throws ResourceNotFoundException if no book exists with the given id,
+     *                                   instead of letting an
+     *                                   EmptyResultDataAccessException escape from
+     *                                   deleteById().
+     */
+    public void deleteById(int id) {
+        if (!bRepo.existsById(id)) {
+            throw new ResourceNotFoundException("Book not found with id: " + id);
+        }
         bRepo.deleteById(id);
     }
 }
--- src/main/java/com/abrar/BOOKSTORE/service/MyBookListService.java
+++ src/main/java/com/abrar/BOOKSTORE/service/MyBookListService.java
@@ -1,6 +1,7 @@
 package com.abrar.BOOKSTORE.service;
 
 import com.abrar.BOOKSTORE.entity.MyBookList;
+import com.abrar.BOOKSTORE.exception.ResourceNotFoundException;
 import com.abrar.BOOKSTORE.repository.MyBookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,14 +13,25 @@ public class MyBookListService {
 
     @Autowired
     private MyBookRepository mybook;
-    public void saveMyBooks(MyBookList book){
+
+    public void saveMyBooks(MyBookList book) {
         mybook.save(book);
     }
-    public List<MyBookList> getAllMyBooks(){
+
+    public List<MyBookList> getAllMyBooks() {
         return mybook.findAll();
 
     }
-    public void deleteById(int id){
+
+    /**
+     * @throws ResourceNotFoundException if no entry exists with the given id,
+     *                                   instead of letting an
+     *                                   EmptyResultDataAccessException escape.
+     */
+    public void deleteById(int id) {
+        if (!mybook.existsById(id)) {
+            throw new ResourceNotFoundException("Book not found in your list with id: " + id);
+        }
         mybook.deleteById(id);
     }
 
