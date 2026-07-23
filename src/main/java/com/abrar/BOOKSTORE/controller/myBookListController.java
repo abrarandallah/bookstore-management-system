ba91d4c2d6420e5df2c9959083c92821a9b40363
abra-adl <sxawra1997@gmail.com>
2026-07-23 21:44:44 +0100

    Handle missing entries in myBookListController

--- src/main/java/com/abrar/BOOKSTORE/controller/myBookListController.java
+++ src/main/java/com/abrar/BOOKSTORE/controller/myBookListController.java
@@ -1,10 +1,13 @@
 package com.abrar.BOOKSTORE.controller;
 
+import com.abrar.BOOKSTORE.exception.ResourceNotFoundException;
 import com.abrar.BOOKSTORE.service.MyBookListService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class myBookListController {
@@ -14,6 +17,12 @@ public class myBookListController {
     @RequestMapping("/deleteMyList/{id}")
     public String deleteMyList(@PathVariable("id") int id) {
         service.deleteById(id);
+        return "redirect:/my_books";
+    }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public String handleResourceNotFound(ResourceNotFoundException ex, RedirectAttributes redirectAttributes) {
+        redirectAttributes.addFlashAttribute("errorMessage", ex.getMessage());
         return "redirect:/my_books";
     }
 }
