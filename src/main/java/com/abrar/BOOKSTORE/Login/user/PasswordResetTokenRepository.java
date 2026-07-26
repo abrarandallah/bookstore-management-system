d6ef328211e18a6d1dd1f124be6dbc6541b0380e
abra-adl <sxawra1997@gmail.com>
2026-07-26 12:17:55 +0100

    fixed librarian in application.properties also added transaction to PasswordResetTokenRepository

--- src/main/java/com/abrar/BOOKSTORE/Login/user/PasswordResetTokenRepository.java
+++ src/main/java/com/abrar/BOOKSTORE/Login/user/PasswordResetTokenRepository.java
@@ -1,11 +1,13 @@
 package com.abrar.BOOKSTORE.Login.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 public interface PasswordResetTokenRepository extends JpaRepository<PasswordResetToken, Long> {
     Optional<PasswordResetToken> findByToken(String token);
 
+    @Transactional
     void deleteByUser(User user);
 }
