02ff5f674a1796e95954a89fef4c538fd6933534
abra-adl <sxawra1997@gmail.com>
2026-07-28 12:25:45 +0100

    Send password reset emails asynchronously

--- src/main/java/com/abrar/BOOKSTORE/Login/auth/EmailService.java
+++ src/main/java/com/abrar/BOOKSTORE/Login/auth/EmailService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -22,6 +23,7 @@ public class EmailService {
     @Value("${spring.mail.username:}")
     private String fromAddress;
 
+    @Async
     public void sendPasswordResetEmail(String toEmail, String token) {
         String link = baseUrl + "/reset-password?token=" + token;
         SimpleMailMessage message = new SimpleMailMessage();
