949493cdb2c66d02c24207a0e66ad95972a1bc9c
abra-adl <sxawra1997@gmail.com>
2026-08-06 01:22:56 +0100

    Remove remaining price param

--- src/main/java/com/abrar/BOOKSTORE/entity/BookTest.java
+++ src/main/java/com/abrar/BOOKSTORE/entity/BookTest.java
@@ -16,11 +16,9 @@ class BookTest {
      * <li>{@link Book#setAuthor(String)}
      * <li>{@link Book#setId(int)}
      * <li>{@link Book#setName(String)}
-     * <li>{@link Book#setPrice(String)}
      * <li>{@link Book#getAuthor()}
      * <li>{@link Book#getId()}
      * <li>{@link Book#getName()}
-     * <li>{@link Book#getPrice()}
      * </ul>
      */
     @Test
@@ -29,45 +27,39 @@ class BookTest {
         actualBook.setAuthor("JaneDoe");
         actualBook.setId(1);
         actualBook.setName("Name");
-        actualBook.setPrice("Price");
         String actualAuthor = actualBook.getAuthor();
         int actualId = actualBook.getId();
         String actualName = actualBook.getName();
         assertEquals("JaneDoe", actualAuthor);
         assertEquals(1, actualId);
         assertEquals("Name", actualName);
-        assertEquals("Price", actualBook.getPrice());
     }
 
     /**
      * Methods under test:
      *
      * <ul>
-     * <li>{@link Book#Book(int, String, String, String)}
+     * <li>{@link Book#Book(int, String, String)}
      * <li>{@link Book#setAuthor(String)}
      * <li>{@link Book#setId(int)}
      * <li>{@link Book#setName(String)}
-     * <li>{@link Book#setPrice(String)}
      * <li>{@link Book#getAuthor()}
      * <li>{@link Book#getId()}
      * <li>{@link Book#getName()}
-     * <li>{@link Book#getPrice()}
      * </ul>
      */
     @Test
     void testConstructor2() {
-        Book actualBook = new Book(1, "Name", "JaneDoe", "Price");
+        Book actualBook = new Book(1, "Name", "JaneDoe");
         actualBook.setAuthor("JaneDoe");
         actualBook.setId(1);
         actualBook.setName("Name");
-        actualBook.setPrice("Price");
         String actualAuthor = actualBook.getAuthor();
         int actualId = actualBook.getId();
         String actualName = actualBook.getName();
         assertEquals("JaneDoe", actualAuthor);
         assertEquals(1, actualId);
         assertEquals("Name", actualName);
-        assertEquals("Price", actualBook.getPrice());
     }
 
     @Test
