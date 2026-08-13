ecbdd6628d35e2794217dad661f555aeca16ef6b
abra-adl <sxawra1997@gmail.com>
2026-08-13 21:03:14 +0100

    the existsByUsernameOrEmail query now also checks the email column

--- src/main/java/com/abrar/BOOKSTORE/Login/user/UserRepository.java
+++ src/main/java/com/abrar/BOOKSTORE/Login/user/UserRepository.java
@@ -14,7 +14,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsernameOrEmail(@Param("usernameOrEmail") String usernameOrEmail);
 
     @Query("SELECT CASE WHEN COUNT(u) > 0 THEN true ELSE false END FROM User u " +
-            "WHERE u.usernameOrEmail = :username OR u.usernameOrEmail = :email")
+            "WHERE u.usernameOrEmail = :username OR u.usernameOrEmail = :email OR u.email = :email")
     boolean existsByUsernameOrEmail(@Param("username") String username, @Param("email") String email);
 
     @Query("SELECT u FROM User u WHERE u.usernameOrEmail = :value OR u.email = :value")
