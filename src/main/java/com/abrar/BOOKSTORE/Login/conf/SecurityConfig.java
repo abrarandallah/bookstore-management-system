e87f93c7f2da3a0ae496ae80bfaadd565d67cecb
abra-adl <sxawra1997@gmail.com>
2026-09-13 00:24:23 +0100

    Remove unused injected field

--- src/main/java/com/abrar/BOOKSTORE/Login/conf/SecurityConfig.java
+++ src/main/java/com/abrar/BOOKSTORE/Login/conf/SecurityConfig.java
@@ -17,7 +17,6 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -29,18 +28,11 @@ import org.springframework.security.web.header.writers.ReferrerPolicyHeaderWrite
 @EnableMethodSecurity
 public class SecurityConfig {
 
-    private final UserDetailsService userDetailsService;
-
     @Autowired
     private CustomUserDetailsService customUserDetailsService;
 
     @Autowired
     private RateLimiter rateLimiter;
-
-    @Autowired
-    public SecurityConfig(UserDetailsService userDetailsService) {
-        this.userDetailsService = userDetailsService;
-    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
